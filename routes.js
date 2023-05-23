16023f074f4a66ef17f7db59b2628d0697fd0d6d
JacqWhales <119764079+JacqWhales@users.noreply.github.com>
2023-05-23 13:19:24 -0400

    turn common route into function and incorporated into logic of addUser route

--- routes.js
+++ routes.js
@@ -86,6 +86,43 @@ module.exports = function(app) {
     });
   }
 
+  async function getCommonData(username) {
+    const [friends, followers] = await Promise.all([
+      getFriends(username),
+      getFollowers(username)
+    ]);
+  
+    const friendsSet = new Set(friends);
+    const common = followers.filter(id => friendsSet.has(id));
+  
+    console.log('common', common.length);
+  
+    const commonData = [];
+    for (let i = 0; i < common.length; i += 100) {
+      const ids = common.slice(i, i + 100).join(',');
+      try {
+        const usersRes = await T.get('users/lookup', { user_id: ids });
+        commonData.push(...usersRes.data.map(data => ({
+          id: data.id_str,
+          name: data.name,
+          screen_name: data.screen_name,
+          description: data.description,
+          profile_image_url: data.profile_image_url_https.replace('_normal', ''),
+          created_at: data.created_at,
+          verified: data.verified,
+          followers_count: data.followers_count,
+          friends_count: data.friends_count
+        })));
+      } catch (err) {
+        console.error(`Error getting user data for IDs: ${ids}`);
+        console.error(err);
+      }
+    }
+  
+    await saveCommonUsersToNeo4j(commonData);
+    return commonData;
+  }
+
 router.get('/common/:username', async (req, res) => {
   try {
     const [friends, followers] = await Promise.all([
@@ -119,7 +156,7 @@ router.get('/common/:username', async (req, res) => {
           console.error(err);
         }
       }
-
+      //only works with users with less than 15k combine following and friends
       await saveCommonUsersToNeo4j(commonData);
       res.status(200).send('Data fetched successfully');
     } catch (err) {
@@ -145,29 +182,19 @@ router.get('/common/:username', async (req, res) => {
   router.get('/session/:sessionName/addUser', async (req, res) => {
     try {
       console.log(`Adding user ${req.query.username} to session ${req.params.sessionName}`);
-      //check if req.query.username is an ethereum address or a twitter username
-      //if ethereum address, get NFT contract data and add to neo4j
+  
       const userInfo = await T.get('users/lookup', { screen_name: req.query.username });
       const userId = userInfo.data[0].id_str;
-      // console.log(`User ID: ${userId}`);
+  
+      const ifUserExists = await db.checkIfUserExists(userId);
+      if (!ifUserExists) {
+        res.status(200).send(`User ${req.query.username} does not exist in database, fetching and saving data now`);
+        await getCommonData(req.query.username);
+      }
+  
       await db.addParticipantToSession(userId, req.params.sessionName);
   
-      // // Prepare the user data to be sent
-      // const userData = {
-      //   id: userInfo.data[0].id_str,
-      //   name: userInfo.data[0].name,
-      //   screen_name: userInfo.data[0].screen_name,
-      //   description: userInfo.data[0].description,
-      //   profile_image_url: userInfo.data[0].profile_image_url_https.replace('_normal', ''),
-      //   created_at: userInfo.data[0].created_at,
-      //   verified: userInfo.data[0].verified,
-      //   followers_count: userInfo.data[0].followers_count,
-      //   friends_count: userInfo.data[0].friends_count
-      // };
-  
-      // // Emit an event to all connected clients
       const io = app.get('io');
-      // io.emit('participant added', { userInfo: userData });
       const newData = await db.addParticipantAndFetchNewData(userId, req.params.sessionName);
       io.emit('new data', newData);
   
