044991fe8013d9ef8399235f4d79be885294c2d7
JacqWhales <119764079+JacqWhales@users.noreply.github.com>
2023-06-08 16:08:27 +0100

    modified cors

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
   cors: {
-    origin: "https://myfrens.xyz",
+    origin: "http://myfrens.xyz",
     methods: ["GET", "POST"]
   }
 });
